03506c02006c23cce98b94dcdee03c67328a1bcf
EldiRangel <eldi456.rangel@gmail.com>
2024-12-01 10:11:36 -0800

    funcion para calcular el angulo

--- angulo_en_radianes.cpp
+++ angulo_en_radianes.cpp
@@ -8,3 +8,11 @@ struct Punto {
     double x;
     double y;
 };
+
+double calcularAngulo(const Punto& a, const Punto& b, const Punto& c, bool enGrados = false) {
+   
+    double abx = b.x - a.x;
+    double aby = b.y - a.y;
+    double bcx = c.x - b.x;
+    double bcy = c.y - b.y;
+}
