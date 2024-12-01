bddbf9d3945da926586ec0a5bf3e96aee662b505
EldiRangel <eldi456.rangel@gmail.com>
2024-12-01 08:15:07 -0800

    desarrollamos la funcion

--- Pares_de_coordenadas.cpp
+++ Pares_de_coordenadas.cpp
@@ -1,2 +1,14 @@
 #include <iostream>
 using namespace std;
+
+struct Puntos {
+    double x, y;
+};
+
+int Interseccion(const Puntos& inicio1, const Puntos& fin1, const Puntos& inicio2, const Puntos& fin2) {
+    double pendiente1 = (fin1.y - inicio1.y) / (fin1.x - inicio1.x);
+    double pendiente2 = (fin2.y - inicio2.y) / (fin2.x - inicio2.x);
+     if (pendiente1 == pendiente2) {
+        
+        return 0;
+    } 
