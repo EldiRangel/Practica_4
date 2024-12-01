6645f51124d07bfa825b7a71e5e9259be73f1264
EldiRangel <eldi456.rangel@gmail.com>
2024-12-01 10:56:25 -0800

    realizando la funcion de void para el proceso

--- llamadas_de_funcion.cpp
+++ llamadas_de_funcion.cpp
@@ -10,3 +10,19 @@ struct Persona {
     string cedula;
     string genero;
 };
+
+void proceso() {
+    contadorLlamadas++;
+
+    Persona p;
+    cout << "Ingrese el nombre: ";
+    cin >> p.nombre;
+    cout << "Ingrese el apellido: ";
+    cin >> p.apellido;
+    cout << "Ingrese la cédula: ";
+    cin >> p.cedula;
+    cout << "Ingrese el género (hombre/mujer): ";
+    cin >> p.genero;
+
+    cout << "Persona ingresada: " << p.nombre << " " << p.apellido << ", Cédula: " << p.cedula << ", Género: " << p.genero << endl;
+}
